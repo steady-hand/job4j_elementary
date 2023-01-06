436c76fd9f925ff04bdf5495537ee844c324668d
steady-hand <71493458+steady-hand@users.noreply.github.com>
2023-01-06 12:44:43 +0300

    Исправление ошибок

---
+++ src/test/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Petr Arsentev");
+        System.out.println("25.01.2002");
+    }
+}
