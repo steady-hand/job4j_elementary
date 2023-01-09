00c51071307de02bd04fdccb1c04aa0183c1fb8e
steady-hand <71493458+steady-hand@users.noreply.github.com>
2023-01-09 17:05:34 +0300

    Операции с математическими функциями

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,17 +1,18 @@
 package ru.job4j.calculator;
 
 public class Calculator {
+    public static void plus(int first, int second) {
+        int result = first + second;
+        System.out.println(result);
+    }
+
     public static void main(String[] args) {
-        int one = 1;
-        int two = 2;
-        int four = 4;
-        int five = 5;
-        int six = 6;
-        int sixDivTwo = six / two;
-        int fourMinusTwo = four - two;
-        int fourTimeTwo = four * two;
-        System.out.println(sixDivTwo);
-        System.out.println(fourMinusTwo);
-        System.out.println(fourTimeTwo);
+        Calculator.plus(100, 500);
+        Calculator.plus(4, 2);
+        Calculator.plus(3, 5);
     }
 }
+
+
+
+
