1b0367e81f3054037b333e51b8b49511ac3d5492
steady-hand <71493458+steady-hand@users.noreply.github.com>
2023-01-18 08:31:51 +0300

    6.0.2. Размер массива [#333582 #66394]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,6 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        System.out.println("Размер массива равен: " + ages.length);
     }
 }
