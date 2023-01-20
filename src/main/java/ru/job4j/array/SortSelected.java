8366e4deb21824bb333874397da72e11fe1dd392
steady-hand <71493458+steady-hand@users.noreply.github.com>
2023-01-20 09:02:08 +0300

    Update

--- src/main/java/ru/job4j/array/SortSelected.java
+++ src/main/java/ru/job4j/array/SortSelected.java
@@ -5,8 +5,7 @@ public class SortSelected {
         for (int i = 0; i < data.length; i++) {
             int min = MinDiapason.findMin(data, i, data.length - 1);
             int index = FindLoop.indexInRange(data, min, i, data.length - 1);
-            data[index] = data[i];
-            data[i] = min;
+            SwitchArray.swap(data, index, i);
         }
         return data;
     }
