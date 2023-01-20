4c22bccc4c4bc93d796509e9d7a217f9fc404fcc
steady-hand <71493458+steady-hand@users.noreply.github.com>
2023-01-20 09:01:48 +0300

    Update

---
+++ src/main/java/ru/job4j/array/Sequence.java
@@ -0,0 +1,16 @@
+package ru.job4j.array;
+
+public class Sequence {
+    public static void main(String[] args) {
+        int[][] array = new int[3][3];
+        array[0][0] = 1;
+        array[0][1] = 2;
+        array[0][2] = 3;
+        array[1][0] = 1;
+        array[1][1] = 2;
+        array[1][2] = 3;
+        array[2][0] = 1;
+        array[2][1] = 2;
+        array[2][2] = 3;
+    }
+}
