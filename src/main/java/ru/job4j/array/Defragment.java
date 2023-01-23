0f7816bc7ee96f6e50e9f251e1e04bff516b24fe
steady-hand <71493458+steady-hand@users.noreply.github.com>
2023-01-23 10:23:38 +0300

    6.8. Дефрагментация массива [#225 #66404]

---
+++ src/main/java/ru/job4j/array/Defragment.java
@@ -0,0 +1,28 @@
+package ru.job4j.array;
+
+public class Defragment {
+    public static String[] compress(String[] array) {
+        for (int index = 0; index < array.length; index++) {
+            if (array[index] == null) {
+                for (int i = 0; i < array.length; i++) {
+                    if (array[index] != null) {
+                        for (int i2 = i + 1; i2 < array.length; i2++) {
+                            array[i] = array[i2];
+                            array[i2] = null;
+                        }
+                    }
+                }
+            }
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
+        String[] compressed = compress(input);
+        System.out.println();
+        for (int index = 0; index < compressed.length; index++) {
+            System.out.print(compressed[index] + " ");
+        }
+    }
+}
